450ee27fd07420663e5d143a601e8cf74a67c92c
Henrik Rudolf <rooodey@gmail.com>
2025-03-07 23:04:43 +0100

    small fixes

--- src/components/search-location.tsx
+++ src/components/search-location.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Expand } from "tailwindcss/types/config";
 import { Typography } from "~/components/typography";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
