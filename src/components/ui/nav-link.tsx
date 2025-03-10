f0a6ca4a46ce5a16d96bd4e1ba53ce7adbb66bba
Henrik Rudolf <rooodey@gmail.com>
2025-03-10 23:53:09 +0100

    scrollIntoView fix

--- src/components/ui/nav-link.tsx
+++ src/components/ui/nav-link.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React, { useEffect, forwardRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { cn } from "~/lib/utils";
 
 export interface NavLinkProps {
@@ -12,41 +12,43 @@ export interface NavLinkProps {
 }
 
 // forwardRef für die richtige Weitergabe des Ref
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
-  ({ href, children, className = "" }, ref) => {
-    const pathname = usePathname();
-    const isActive = pathname === href;
+export default function NavLink({
+  href,
+  children,
+  className = "",
+}: NavLinkProps) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+  const navLinkRef = useRef<HTMLAnchorElement>(null);
 
-    useEffect(() => {
-      // Sicherstellen, dass ref.current existiert und richtig genutzt wird
-      if (isActive && ref && typeof ref !== "function" && ref.current) {
-        ref.current.scrollIntoView({
-          behavior: "auto",
-          inline: "nearest",
-        });
-      }
-    }, [isActive, ref]);
+  useEffect(() => {
+    if (
+      isActive &&
+      navLinkRef &&
+      typeof navLinkRef !== "function" &&
+      navLinkRef.current
+    ) {
+      navLinkRef.current.scrollIntoView({
+        behavior: "auto",
+        inline: "center",
+      });
+    }
+  }, [isActive, navLinkRef]);
 
-    return (
-      <Link href={href} legacyBehavior>
-        <a
-          ref={ref} // Ref wird hier korrekt weitergegeben
-          className={cn(
-            "no-scroll-animation fix-ios-render whitespace-nowrap pb-2 transition-colors",
-            isActive
-              ? "z-50 border-b border-primary font-semibold text-primary"
-              : "text-muted-foreground",
-            className,
-          )}
-        >
-          {children}
-        </a>
-      </Link>
-    );
-  },
-);
-
-// forwardRef benötigt eine Display-Name-Zuweisung für Debugging-Zwecke
-NavLink.displayName = "NavLink";
-
-export default NavLink;
+  return (
+    <Link href={href} legacyBehavior>
+      <a
+        ref={navLinkRef}
+        className={cn(
+          "no-scroll-animation fix-ios-render whitespace-nowrap pb-2 transition-colors",
+          isActive
+            ? "z-50 border-b border-primary font-semibold text-primary"
+            : "text-muted-foreground",
+          className,
+        )}
+      >
+        {children}
+      </a>
+    </Link>
+  );
+}
