8febd34fc1c34deaf93bb502b227cc676f04e2e8
Henrik R <henrik.rudolf@gmx.net>
2025-03-19 15:02:06 +0100

    small changes

--- src/app/create-venue/page.tsx
+++ src/app/create-venue/page.tsx
@@ -1,12 +1,12 @@
-import { TableForm } from "~/app/create-table/components/table-form";
 import AppContainer from "~/components/app-container";
 import AppHeader from "~/components/app-header";
+import { VenueForm } from "./components/venue-form";
 
 export default async function Page() {
   return (
     <AppContainer>
       <AppHeader title="Create Venue">
-        <TableForm />
+        <VenueForm />
       </AppHeader>
     </AppContainer>
   );
